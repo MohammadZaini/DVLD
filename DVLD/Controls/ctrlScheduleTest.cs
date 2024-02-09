851d8073db3ece2d658dd64de34a003dcbc73db0
Mohammad Zaini <Mohammad_zaini@outlook.com>
2024-02-09 22:29:37 +0300

    Adding some functionality to Schedule Test UserControl

--- DVLD/Controls/ctrlScheduleTest.cs
+++ DVLD/Controls/ctrlScheduleTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static DVLD_Business.clsPerson;
 
 namespace DVLD.Controls
 {
@@ -19,6 +20,9 @@ namespace DVLD.Controls
 
         private enTestType _testType = enTestType.Vision;
         private enTestMode _testMode = enTestMode.FirstTimeTest;
+        private clsTestAppointment _testAppointment;
+        private int _localDrivingLicenseAppID;
+        private int _testAppointmentID;
 
         public DateTime ApplicationDate { get; set; }
         public decimal PaidFees { get; set; }
@@ -27,23 +31,36 @@ namespace DVLD.Controls
             InitializeComponent();
         }
 
-        public void LoadTestAppointmentDetails(int drivingLicenseAppID, int testType)
+        public void LoadTestAppointmentDetails(int drivingLicenseAppID, int testType, int testMode, int testAppoinmentID = 1)
         {
-            lblDrivingLicenseAppID.Text = drivingLicenseAppID.ToString();
-            dtpTestAppointentDate.Value = ApplicationDate;
-
+            _testAppointmentID = testAppoinmentID;
+            _localDrivingLicenseAppID = drivingLicenseAppID;
+            
             _testType = (enTestType)testType;
+            _testMode = (enTestMode)testMode;
 
             clsLocalLicenseApplication localLicenseApplication = clsLocalLicenseApplication.Find(drivingLicenseAppID);
+
             if (localLicenseApplication == null)
                 return;
 
+            // Initialize Test Appointment object
+            _InitializeTestAppointmentObject();
+
+            // Update the Test Type Photo based on Test Type
             _UpdateTestTypePhoto();
+
+            // Toggle The test Appointment mode based on the Mode: FirstTime / Edit / Retake
+            _ToggleTestAppointmentMode(testMode);
+
+            // Update the test Appointment UI
             _UpdateAppointmentDetails(localLicenseApplication);
         }
 
         private void _UpdateAppointmentDetails(clsLocalLicenseApplication localLicenseApplication)
         {
+            lblDrivingLicenseAppID.Text = _localDrivingLicenseAppID.ToString();
+            dtpTestAppointentDate.Value = ApplicationDate;
             lblLicenseClass.Text = localLicenseApplication.LicenseClassName;
             lblApplicantName.Text = localLicenseApplication.ApplicantFullName;
             lblTrial.Text = clsLocalLicenseApplication.FailureCount(localLicenseApplication.LocalLicenseApplicationID).ToString();
@@ -109,6 +126,79 @@ namespace DVLD.Controls
                     break;
             }
 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_testAppointment == null)
+                return;
+
+            _InitializeTestAppointment();
+
+            if (clsTestAppointment.IsPersonFailed(_localDrivingLicenseAppID))
+            {
+                _CreateNewRetakeTestApplication();
+            }
+
+            if (_testAppointment.Save())
+                MessageBox.Show("Data Saved Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Something went wrong", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void _InitializeTestAppointment()
+        {
+            _testAppointment.TestTypeID = 1; // Vision Test
+            _testAppointment.LocalDrivingLicenseApplicationID = _localDrivingLicenseAppID;
+            _testAppointment.AppointmentDate = ApplicationDate;
+            _testAppointment.PaidFees = PaidFees;
+            _testAppointment.CreatedByUserID = clsGlobalSettings.LoggedInUser.UserID;
+            _testAppointment.IsLocked = false;
+
+        }
+
+        private void _InitializeTestAppointmentObject()
+        {
+            _testAppointment = (_testMode == enTestMode.FirstTimeTest) ? new clsTestAppointment() : clsTestAppointment.Find(_testAppointmentID);
+        }
+
+        private void _CreateNewRetakeTestApplication()
+        {
+            // current localLicenseApplication to get the applicant person ID 
+            clsLocalLicenseApplication currentLocalLicenseApplication = _CurrentLocalLicenseApplication(_localDrivingLicenseAppID);
+            int applicantPersonID = currentLocalLicenseApplication.Application.ApplicantPersonID;
+
+            const int retakeTestTypeID = 8;
+            const int appStatusNew = 1;
+
+            // create a new retake test type and connect it with the current Local License Application
+            clsApplication newApplication = _InitializeNewRetakeTestApplication(applicantPersonID, retakeTestTypeID, appStatusNew);
+
+            int applicationID = newApplication.AddNewApplication();
+            _testAppointment.RetakeTestApplicationID = applicationID;
+        }
+
+        private clsLocalLicenseApplication _CurrentLocalLicenseApplication(int localDrivingLicenseAppID)
+        {
+            return clsLocalLicenseApplication.Find(localDrivingLicenseAppID);
+        }
+
+        private clsApplication _InitializeNewRetakeTestApplication(int applicantPersonID, int retakeTestTypeID, int appStatusNew)
+        {
+
+            clsApplication newApplication = new clsApplication();
+
+            newApplication.ApplicantPersonID = applicantPersonID;          
+            newApplication.ApplicationStatus = appStatusNew;
+            newApplication.ApplicationTypeID = retakeTestTypeID;
+            newApplication.ApplicationDate = DateTime.Now;
+            newApplication.LastStatusDate = DateTime.Now;
+
+            clsApplicationType testType = clsApplicationType.Find(retakeTestTypeID);
+            newApplication.PaidFees = testType.AppFees;
+            newApplication.CreatedByUserID = clsGlobalSettings.LoggedInUser.UserID;
+
+            return newApplication;
         }
     }
 }
